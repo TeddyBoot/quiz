e825195c79281d39dab7dccc2bdadca440577056
boottedd <boottedd@lenlt001.rhce.lan>
2016-12-14 09:50:24 +0100

    First question add page with checks almost finished.

--- includes/scripts/form.js
+++ includes/scripts/form.js
@@ -1,5 +1,4 @@
 function handleSelection(choice,variable1,variable2) {
-	//document.getElementById('select').disabled=true;
 	if(choice==variable1) {
 		document.getElementById(variable1).style.display='table-row';
 		document.getElementById(variable2).style.display='none';
@@ -18,8 +17,53 @@ function addInput(divName){
      }
      else {
           var newdiv = document.createElement('div');
-          newdiv.innerHTML = "<textarea name='myInputs[]'>This is " + (counter + 1) + "</textarea><br />";
+          newdiv.innerHTML = "<textarea name='myInputs["+counter+"]' placeholder=\"Incorrect awnser here\"></textarea><br />";
           document.getElementById(divName).appendChild(newdiv);
           counter++;
      }
+}
+
+function checkFromEntries(){
+	// check if the correct answer is filled
+	var selectForm = document.getElementById("select");
+	if (selectForm[selectForm.selectedIndex].value == "a1") {
+		var ans1 = document.getElementById("answer1");
+		if (ans1.value.length < 1) {
+			alert("Awnser has not been filled");
+			return false;
+		}
+	} else if (selectForm[selectForm.selectedIndex].value == "a2") {
+		// check if the correct answer is filled
+		alert("The value is = "+selectForm[selectForm.selectedIndex].value);
+		var ans2 = document.addquest.elements["myInputs[0]"];
+		var ans3 = document.addquest.elements["myInputs[1]"];
+		if (ans2.value == "") {
+			alert("The correct answer has not been filled");
+			return false;
+		} else if (ans3 == undefined) {
+			// check if at least one wrong answer has been filled
+			alert("At least one wrong answer has to be filled!");
+			return false;
+		} else if (ans3.value == "") {
+			// check if at least one wrong answer has been filled
+			alert("At least one wrong answer has to be filled");
+			return false;
+		}
+	}
+	// check if at least one school level has been selected
+	var nonScholar = document.addquest.nonScholar.value;
+	if (nonScholar == "school1") {
+		var tmpX = 0;
+		for (var i = 0; i < 15; i++) {
+		    var x = "lv["+i+"]";
+		    var y = document.addquest.elements[x];
+		    if (y.checked == true) {
+		    	tmpX++;
+		    }
+		}
+		if (tmpX < 1) {
+			alert("no school level selected");
+			return false;
+		}
+	}
 }
